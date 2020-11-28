e1d1ba0c659bda604dfceee84cc8f965376ea2c3
Stephane Demotte <stephane@demotte.net>
2020-11-27 20:43:10 -0500

    big bang

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>{{ isActive.value }}(work in build)</div>
+  <div>{{ isActive }}(work in dev)</div>
+  <button @click="isActive.value = !isActive.value">change value</button>
+  <HelloWorld />
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import { isActive } from './components/HelloWorld.vue'
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    {{ isActiveTpl }}
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export const isActive = ref(false)
+</script>
+
+<script setup>
+const isActiveTpl = isActive
+</script>
